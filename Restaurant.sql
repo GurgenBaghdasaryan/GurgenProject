074be3a4a6a0e0b1aa45612950d1c3e2e30b1cce
GurgenBaghdasaryan <grg.baghdasaryan@gmail.com>
2020-12-10 22:16:47 +0400

    Added Restaurant DB

--- Restaurant.sql
+++ Restaurant.sql
@@ -1,10 +1,12 @@
 CREATE DATABASE Restaurant;
 USE Restaurant;
+
 CREATE TABLE IF NOT EXISTS Salary (
     id INT NOT NULL AUTO_INCREMENT,
     salary DECIMAL(18, 4),
     PRIMARY KEY(id)
   );
+  
 CREATE TABLE IF NOT EXISTS Employee (
     id INT NOT NULL AUTO_INCREMENT,
     firstName VARCHAR(20),
@@ -15,6 +17,7 @@ CREATE TABLE IF NOT EXISTS Employee (
     PRIMARY KEY(id),
     FOREIGN KEY (salaryId) REFERENCES Salary(id)
 );
+
 CREATE TABLE IF NOT EXISTS Menu (
     id INT NOT NULL AUTO_INCREMENT,
     breakfast VARCHAR(20),
@@ -23,20 +26,26 @@ CREATE TABLE IF NOT EXISTS Menu (
     humburgers VARCHAR(20),
     PRIMARY KEY(id)
 );
+
 CREATE TABLE IF NOT EXISTS Product (
     id INT NOT NULL AUTO_INCREMENT,
     productName VARCHAR(20),
     productPrice DECIMAL(18,4),
     PRIMARY KEY(id)
 );
+
 INSERT INTO Salary (salary)
 VALUES (23.789);
+
 INSERT INTO Employee (firstName, lastName, age, salaryId)
 VALUES ('Gurgen','Bahgdasaryan',17,1);
+
 INSERT INTO Menu (breakfast,appetizers, lunch,humburgers)
 VALUES ('eggs','cheese','dumplings','chicken');
+
 INSERT INTO Product (productName, productPrice)
 VALUES ('eggs', 25.968);
+
 SELECT Employee.id, Employee.firstName,Salary.salary
 FROM Employee
 INNER JOIN Salary ON Employee.salaryId=Salary.id;
