0d7fa64ad159f4dfb817a9c52bf5df5a949d4d37
GurgenBaghdasaryan <grg.baghdasaryan@gmail.com>
2020-12-10 21:08:23 +0400

    updated the code

--- Restaurant.sql
+++ Restaurant.sql
@@ -1,21 +1,22 @@
 CREATE DATABASE Restaurant;
 USE Restaurant;
 CREATE TABLE IF NOT EXISTS Salary (
-    id INT AUTO_INCREMENT,
-    salary INT,
+    id INT NOT NULL AUTO_INCREMENT,
+    salary DECIMAL(18, 4),
     PRIMARY KEY(id)
   );
 CREATE TABLE IF NOT EXISTS Employee (
-    id INT AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT,
     firstName VARCHAR(20),
     lastName VARCHAR(20),
     age INT,
-    salaryId INT,
+    CHECK (age>=0 AND age <= 200),
+    salaryId INT NOT NULL,
     PRIMARY KEY(id),
     FOREIGN KEY (salaryId) REFERENCES Salary(id)
 );
 CREATE TABLE IF NOT EXISTS Menu (
-    id INT AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT,
     breakfast VARCHAR(20),
     appetizers VARCHAR(20),
     lunch VARCHAR(20),
@@ -23,19 +24,19 @@ CREATE TABLE IF NOT EXISTS Menu (
     PRIMARY KEY(id)
 );
 CREATE TABLE IF NOT EXISTS Product (
-    id INT AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT,
     productName VARCHAR(20),
-    productPrice int,
+    productPrice DECIMAL(18,4),
     PRIMARY KEY(id)
 );
 INSERT INTO Salary (salary)
-VALUES (10000);
+VALUES (23.789);
 INSERT INTO Employee (firstName, lastName, age, salaryId)
 VALUES ('Gurgen','Bahgdasaryan',17,1);
 INSERT INTO Menu (breakfast,appetizers, lunch,humburgers)
 VALUES ('eggs','cheese','dumplings','chicken');
-INSERT INTO Product (productName,productPrice)
-VALUES ('eggs',70);
-SELECT Employee.id, Employee.firstName,Employee.salaryId
+INSERT INTO Product (productName, productPrice)
+VALUES ('eggs', 25.968);
+SELECT Employee.id, Employee.firstName,Salary.salary
 FROM Employee
-INNER JOIN salary ON employee.salaryId=Salary.id;
+INNER JOIN Salary ON Employee.salaryId=Salary.id;
