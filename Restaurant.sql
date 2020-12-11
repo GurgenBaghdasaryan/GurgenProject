9dc0164c7d19d843e6aa47af497b3d72354f4fdc
GurgenBaghdasaryan <grg.baghdasaryan@gmail.com>
2020-12-11 21:53:22 +0400

    improved solution

--- Restaurant.sql
+++ Restaurant.sql
@@ -1,51 +1,36 @@
 CREATE DATABASE Restaurant;
 USE Restaurant;
-
-CREATE TABLE IF NOT EXISTS Salary (
+CREATE TABLE IF NOT EXISTS salary (
     id INT NOT NULL AUTO_INCREMENT,
     salary DECIMAL(18, 4),
+    job VARCHAR(20),
     PRIMARY KEY(id)
   );
-
-CREATE TABLE IF NOT EXISTS Employee (
+CREATE TABLE IF NOT EXISTS menu_set (
     id INT NOT NULL AUTO_INCREMENT,
-    firstName VARCHAR(20),
-    lastName VARCHAR(20),
+    set_name VARCHAR(100),
+    set_price DECIMAL(18, 4),
+    PRIMARY KEY(id)
+);
+CREATE TABLE IF NOT EXISTS customer (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(20),
+    last_name VARCHAR(20),
     age INT,
-    salaryId INT NOT NULL,
+    salary_id INT NOT NULL,
+    ordered_set_id INT NOT NULL,
     PRIMARY KEY(id),
     CHECK (age>=0 AND age <= 200),
-    FOREIGN KEY (salaryId) REFERENCES Salary(id)
+    FOREIGN KEY (salary_id) REFERENCES salary(id),
+    FOREIGN KEY (ordered_set_id) REFERENCES menu_set(id)
 );
-
-CREATE TABLE IF NOT EXISTS Menu (
-    id INT NOT NULL AUTO_INCREMENT,
-    breakfast VARCHAR(20),
-    appetizers VARCHAR(20),
-    lunch VARCHAR(20),
-    humburgers VARCHAR(20),
-    PRIMARY KEY(id)
-);
-
-CREATE TABLE IF NOT EXISTS Product (
-    id INT NOT NULL AUTO_INCREMENT,
-    productName VARCHAR(20),
-    productPrice DECIMAL(18,4),
-    PRIMARY KEY(id)
-);
-
-INSERT INTO Salary (salary)
-VALUES (23.789);
-
-INSERT INTO Employee (firstName, lastName, age, salaryId)
-VALUES ('Gurgen','Bahgdasaryan',17,1);
-
-INSERT INTO Menu (breakfast,appetizers, lunch,humburgers)
-VALUES ('eggs','cheese','dumplings','chicken');
-
-INSERT INTO Product (productName, productPrice)
-VALUES ('eggs', 25.968);
-
-SELECT Employee.id, Employee.firstName,Salary.salary
-FROM Employee
-INNER JOIN Salary ON Employee.salaryId=Salary.id;
+INSERT INTO salary (salary,job)
+VALUES (23.789,'Programmer');
+INSERT INTO menu_set (set_name,set_price)
+VALUES ('Hamburger, Fryes, Cola',13.768);
+INSERT INTO customer (first_name, last_name, age, salary_id, ordered_set_id)
+VALUES ('Gurgen','Bahgdasaryan',17,1,1);
+SELECT customer.id, customer.first_name, salary.salary, menu_set.set_name, menu_set.set_price
+FROM customer
+INNER JOIN salary ON salary.id = customer.salary_id
+INNER JOIN menu_set ON customer.ordered_set_id = menu_set.id
